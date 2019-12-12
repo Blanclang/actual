5791146ff8c7965e55c8bc3ed1ca0f8bcdfce3c7
bailang <bail@zhengtongauto.com>
2019-12-12 16:06:47 +0800

    添加babel

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -14,6 +14,16 @@ module.exports={
   module:{
     rules:[
       {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
         test:/\.css$/,
         use:[MiniCssExtractPlugin.loader,'css-loader','postcss-loader']
       },
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './app.css';
 import './index.less';
-
+import { add } from './test';
+add(1,2);
 console.log('123');
